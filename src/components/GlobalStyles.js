e89bf3dcdbb2e4dddad8abe3caa8393bb3192d1a
seokh1213 <seokh1213@gmail.com>
2020-06-12 14:55:46 +0900

    chore: css 폰트 설정

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,6 +3,7 @@ import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
   ${reset};
+  @import url(http://fonts.googleapis.com/earlyaccess/notosanskr.css); 
   a{
       text-decoration:none;
       color:inherit;
@@ -11,7 +12,7 @@ const GlobalStyles = createGlobalStyle`
       box-sizing:border-box;
   }
   body{
-      font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      font-family:'Noto Sans KR', BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
       font-size: 14px;
   }
 `;
